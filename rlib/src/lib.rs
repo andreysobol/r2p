796d669b6d910c0ac914c2ab1f4d43505d10261c
Andrey Sobol <baskerville@andreysobol.com>
2019-12-19 05:11:33 +0200

    create lib

--- rlib/src/lib.rs
+++ rlib/src/lib.rs
@@ -1,7 +1,13 @@
 #[macro_use] extern crate cpython;
 use cpython::{Python, PyResult};
 
-fn hello(py: Python) -> PyResult<()> {
+fn hello(py: Python, val:u64) -> PyResult<u64> {
     println!("This is Rust");
-    Ok(())
+    Ok(0u64)
 }
+
+py_module_initializer!(rlib, initrlib, PyInit_rlib, |py, m | {
+    try!(m.add(py, "__doc__", "This module is implemented in Rust"));
+    try!(m.add(py, "hellot", py_fn!(py, hello(val:u64))));
+    Ok(())
+});
